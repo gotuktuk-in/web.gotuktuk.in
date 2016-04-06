9726e58549148d4087156b46f6990606b2be08e0
Sunil <sunil.chaurasia@47billion.com>
2016-04-06 19:08:47 +0530

    Added New Changes

--- tCash/js/scrollpagination.js
+++ tCash/js/scrollpagination.js
@@ -12,7 +12,7 @@
 	 
 		 
  $.fn.scrollPagination = function(options) {
-  	
+
 		var opts = $.extend($.fn.scrollPagination.defaults, options);  
 		var target = opts.scrollTarget;
 		if (target == null){
@@ -34,6 +34,12 @@
   };
   
   $.fn.scrollPagination.loadContent = function(obj, opts){
+	  var books = [
+		  { title: "ASP.NET 4 Unleashed", price: 37.79, picture: "AspNet4Unleashed.jpg" },
+		  { title: "ASP.NET MVC Unleashed", price: 44.99, picture: "AspNetMvcUnleashed.jpg" },
+		  { title: "ASP.NET Kick Start", price: 4.00, picture: "AspNetKickStart.jpg" },
+		  { title: "ASP.NET MVC Unleashed iPhone", price: 44.99, picture: "AspNetMvcUnleashedIPhone.jpg" },
+	  ];
 	 var target = opts.scrollTarget;
 	 var mayLoadContent = $(target).scrollTop()+opts.heightOffset >= $(document).height() - $(target).height();
 	 if (mayLoadContent){
@@ -48,9 +54,11 @@
 				  'content-type':'application/json'
 			  },
 			  url: opts.contentPage,
-			//  data: opts.contentData,
+			  data: opts.contentData,
 			  success: function(data){
-				$(obj).append(data); 
+				//$(obj).append(data);
+				  var newData =JSON.parse(data)
+				  $("#tCashTemplate").tmpl(newData).appendTo("#content");
 				var objectsRendered = $(obj).children('[rel!=loaded]');
 				
 				if (opts.afterLoad != null){
@@ -89,3 +97,19 @@
 		 'heightOffset': 0		  
  };	
 })( jQuery );
+function formattedDate(date) {
+	var d = new Date(date*1000 || Date.now()),
+		month = '' + GetMonthName((d.getMonth() + 1)),
+		day = '' + d.getDate(),
+		year = d.getFullYear();
+
+	if (month.length < 2) month = '0' + month;
+	if (day.length < 2) day = '0' + day;
+
+	return [day, month, year].join('/');
+}
+
+function GetMonthName(monthNumber) {
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	return months[monthNumber - 1];
+}
